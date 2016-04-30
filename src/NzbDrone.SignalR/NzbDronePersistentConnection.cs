662ea79b05765704759f3c46be6f39a977563b50
Keivan Beigi <me@keivan.io>
2016-04-30 13:04:14 -0700

    SignalR cleanup

--- src/NzbDrone.SignalR/NzbDronePersistentConnection.cs
+++ src/NzbDrone.SignalR/NzbDronePersistentConnection.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Infrastructure;
+using NzbDrone.Common.Serializer;
 
 namespace NzbDrone.SignalR
 {
@@ -12,8 +14,27 @@ namespace NzbDrone.SignalR
     {
         private IPersistentConnectionContext Context => ((ConnectionManager)GlobalHost.ConnectionManager).GetConnection(GetType());
 
+        private readonly Dictionary<string, string> _messageHistory; 
+
+        public NzbDronePersistentConnection()
+        {
+            _messageHistory = new Dictionary<string, string>();
+        }
+
+
         public void BroadcastMessage(SignalRMessage message)
         {
+            string lastMessage;
+            if (_messageHistory.TryGetValue(message.Name, out lastMessage))
+            {
+                if (message.Body.ToJson() == lastMessage)
+                {
+                    return;
+                }
+            }
+
+            _messageHistory[message.Name] = message.Body.ToJson();
+
             Context.Connection.Broadcast(message);
         }
     }
