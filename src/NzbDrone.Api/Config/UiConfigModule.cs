4a6d479ef02c5fd7dfd5645bd203d7aa415ab7d3
Mark McDowall <markus.mcd5@gmail.com>
2016-07-30 01:15:05 -0700

    Fixes after rebase

--- src/NzbDrone.Api/Config/UiConfigModule.cs
+++ src/NzbDrone.Api/Config/UiConfigModule.cs
@@ -1,9 +1,7 @@
 ﻿using NzbDrone.Core.Configuration;
-using Sonarr.Http;
 
 namespace NzbDrone.Api.Config
 {
-    public class UiConfigModule : SonarrRestModule<UiConfigResource>
     public class UiConfigModule : NzbDroneConfigModule<UiConfigResource>
     {
         public UiConfigModule(IConfigService configService)
