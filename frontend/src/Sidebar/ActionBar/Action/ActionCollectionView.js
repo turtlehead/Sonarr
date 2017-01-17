aa6d72dce0a314ca1864bca9b2fbc5219ff2e979
Mark McDowall <markus.mcd5@gmail.com>
2017-01-16 21:45:24 -0800

    fixup! Goodbye marionette, hello react router

--- frontend/src/Sidebar/ActionBar/Action/ActionCollectionView.js
+++
@@ -1,8 +0,0 @@
-const Marionette = require('marionette');
-const ActionView = require('./ActionView');
-
-module.exports = Marionette.CollectionView.extend({
-  tagName: 'ul',
-  className: 'actionbar-list',
-  childView: ActionView
-});
--- frontend/src/Sidebar/ActionBar/ButtonCollection.js
+++
@@ -1,6 +0,0 @@
-const Backbone = require('backbone');
-const ButtonModel = require('./ButtonModel');
-
-module.exports = Backbone.Collection.extend({
-  model: ButtonModel
-});
