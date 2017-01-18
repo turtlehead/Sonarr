6afbe78eef34da0826c874353a1424d70a260d25
Mark McDowall <markus.mcd5@gmail.com>
2017-01-18 09:15:19 -0800

    Update iCal feed module

--- src/Sonarr.Api.V3/Calendar/CalendarFeedModule.cs
+++ src/Sonarr.Api.V3/Calendar/CalendarFeedModule.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using DDay.iCal;
+using Ical.Net;
+using Ical.Net.DataTypes;
+using Ical.Net.Interfaces.Serialization;
+using Ical.Net.Serialization;
+using Ical.Net.Serialization.iCalendar.Factory;
 using Nancy;
 using Nancy.Responses;
+using NzbDrone.Common.Extensions;
+using NzbDrone.Core.Tags;
 using NzbDrone.Core.Tv;
 
 namespace Sonarr.Api.V3.Calendar
@@ -11,13 +17,15 @@ namespace Sonarr.Api.V3.Calendar
     public class CalendarFeedModule : SonarrV3FeedModule
     {
         private readonly IEpisodeService _episodeService;
+        private readonly ITagService _tagService;
 
-        public CalendarFeedModule(IEpisodeService episodeService)
+        public CalendarFeedModule(IEpisodeService episodeService, ITagService tagService)
             : base("calendar")
         {
             _episodeService = episodeService;
+            _tagService = tagService;
 
-            Get["/NzbDrone.ics"] = options => GetCalendarFeed();
+            Get["/Sonarr.ics"] = options => GetCalendarFeed();
         }
 
         private Response GetCalendarFeed()
@@ -26,9 +34,17 @@ namespace Sonarr.Api.V3.Calendar
             var futureDays = 28;            
             var start = DateTime.Today.AddDays(-pastDays);
             var end = DateTime.Today.AddDays(futureDays);
+            var unmonitored = false;
+            var premiersOnly = false;
+            var asAllDay = false;
+            var tags = new List<int>();
 
             var queryPastDays = Request.Query.PastDays;
             var queryFutureDays = Request.Query.FutureDays;
+            var queryUnmonitored = Request.Query.Unmonitored;
+            var queryPremiersOnly = Request.Query.PremiersOnly;
+            var queryAsAllDay = Request.Query.AsAllDay;
+            var queryTags = Request.Query.Tags;
 
             if (queryPastDays.HasValue)
             {
@@ -42,33 +58,75 @@ namespace Sonarr.Api.V3.Calendar
                 end = DateTime.Today.AddDays(futureDays);
             }
 
-            var episodes = _episodeService.EpisodesBetweenDates(start, end, false);
-            var icalCalendar = new iCalendar();
+            if (queryUnmonitored.HasValue)
+            {
+                unmonitored = bool.Parse(queryUnmonitored.Value);
+            }
+
+            if (queryPremiersOnly.HasValue)
+            {
+                premiersOnly = bool.Parse(queryPremiersOnly.Value);
+            }
+
+            if (queryAsAllDay.HasValue)
+            {
+                asAllDay = bool.Parse(queryAsAllDay.Value);
+            }
+
+            if (queryTags.HasValue)
+            {
+                var tagInput = (string)queryTags.Value.ToString();
+                tags.AddRange(tagInput.Split(',').Select(_tagService.GetTag).Select(t => t.Id));
+            }
+
+            var episodes = _episodeService.EpisodesBetweenDates(start, end, unmonitored);
+            var calendar = new Ical.Net.Calendar
+            {
+                ProductId = "-//sonarr.tv//Sonarr//EN"
+            };
+
 
             foreach (var episode in episodes.OrderBy(v => v.AirDateUtc.Value))
             {
-                var occurrence = icalCalendar.Create<Event>();
-                occurrence.UID = "NzbDrone_episode_" + episode.Id.ToString();
+                if (premiersOnly && (episode.SeasonNumber == 0 || episode.EpisodeNumber != 1))
+                {
+                    continue;
+                }
+
+                if (tags.Any() && tags.None(episode.Series.Tags.Contains))
+                {
+                    continue;
+                }
+
+                var occurrence = calendar.Create<Event>();
+                occurrence.Uid = "NzbDrone_episode_" + episode.Id;
                 occurrence.Status = episode.HasFile ? EventStatus.Confirmed : EventStatus.Tentative;
-                occurrence.Start = new iCalDateTime(episode.AirDateUtc.Value) { HasTime = true };
-                occurrence.End = new iCalDateTime(episode.AirDateUtc.Value.AddMinutes(episode.Series.Runtime)) { HasTime = true };
                 occurrence.Description = episode.Overview;
                 occurrence.Categories = new List<string>() { episode.Series.Network };
+
+                if (asAllDay)
+                {
+                    occurrence.Start = new CalDateTime(episode.AirDateUtc.Value) { HasTime = false };
+                }
+                else
+                {
+                    occurrence.Start = new CalDateTime(episode.AirDateUtc.Value) { HasTime = true };
+                    occurrence.End = new CalDateTime(episode.AirDateUtc.Value.AddMinutes(episode.Series.Runtime)) { HasTime = true };
+                }
 
                 switch (episode.Series.SeriesType)
                 {
                     case SeriesTypes.Daily:
-                        occurrence.Summary = string.Format("{0} - {1}", episode.Series.Title, episode.Title);
+                        occurrence.Summary = $"{episode.Series.Title} - {episode.Title}";
                         break;
-
                     default:
-                        occurrence.Summary = string.Format("{0} - {1}x{2:00} - {3}", episode.Series.Title, episode.SeasonNumber, episode.EpisodeNumber, episode.Title);
+                        occurrence.Summary = $"{episode.Series.Title} - {episode.SeasonNumber}x{episode.EpisodeNumber:00} - {episode.Title}";
                         break;
                 }
             }
 
-            var serializer = new DDay.iCal.Serialization.iCalendar.SerializerFactory().Build(icalCalendar.GetType(), new DDay.iCal.Serialization.SerializationContext()) as DDay.iCal.Serialization.IStringSerializer;
-            var icalendar = serializer.SerializeToString(icalCalendar);
+            var serializer = (IStringSerializer)new SerializerFactory().Build(calendar.GetType(), new SerializationContext());
+            var icalendar = serializer.SerializeToString(calendar);
 
             return new TextResponse(icalendar, "text/calendar");
         }
